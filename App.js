c0865127819c6e5ca4b0002bc73b2a2480a7c740
dev <dev@iMac-de-alex.local>
2023-02-23 12:06:27 +0100

    v0.3.0 : étape 3 : ajout et suppression de tâche

--- App.js
+++ App.js
@@ -1,52 +1,90 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, FlatList, TextInput, Button } from 'react-native';
+
+import {
+    StyleSheet,
+    Text,
+    View,
+    FlatList,
+    TextInput,
+    Button,
+    StatusBar,
+    TouchableOpacity
+} from 'react-native';
 import React
   from "react";
 
 
 export default function App() {
-  const sampleGoals = [
-    "Faire les courses",
-    "Aller à la salle de sport 3 fois par semaine",
-    "Monter à plus de 5000m d altitude",
-    "Acheter mon premier appartement",
-    "Perdre 5 kgs",
-    "Gagner en productivité",
-    "Apprendre un nouveau langage",
-    "Faire une mission en freelance",
-    "Organiser un meetup autour de la tech",
-    "Faire un triathlon",
-  ];
 
-  const renderItem = ({ item }) => (
-      <Text>{item}</Text>)
+    const [goal, setGoal] = React.useState(
+        [
+            "Faire les courses",
+            "Aller à la salle de sport 3 fois par semaine",
+            "Monter à plus de 5000m d altitude",
+            "Acheter mon premier appartement",
+            "Perdre 5 kgs",
+            "Gagner en productivité",
+            "Apprendre un nouveau langage",
+            "Faire une mission en freelance",
+            "Organiser un meetup autour de la tech",
+            "Faire un triathlon",
+        ]
+    );
+    const [newGoal, setNewGoal] = React.useState("");
 
-  const [text, onChangeText] = React.useState('');
+  // fonction qui va render la ligne
+  const renderItem = ({ index, item }) => (
+      <View style={{ flexDirection:'row' }}>
+      <Text>{item} </Text>
+          <TouchableOpacity onPress={()=> removeGoal(index)}>
+              <Text>X</Text>
+          </TouchableOpacity>
+      </View>
+  );
+
+  // fonction qui prend en charge l'input
+    const textChange = (text) => {
+        setNewGoal(text);
+    };
+
+    // fonction qui va ajouter la tâche
+    const addGoal = () => {
+        setGoal([...goal, newGoal]);
+        setNewGoal("");
+    }
+
+    //fonction qui va effacer la tâche
+    const removeGoal = (index) => {
+        setGoal((prevState) => {
+            const newState = [...prevState];
+            newState.splice(index, 1);
+            return newState;
+        });
+    };
 
   return (
+      <>
+          <StatusBar style="auto"></StatusBar>
+          <View style={styles.container}>
+              <Text style={styles.title}>App.js</Text>
+              <FlatList
+                  data={goal}
+                  renderItem={renderItem}
+              />
+
+          </View>
+          <View style={{ flexDirection:'row' }}>
+              <TextInput
+                  style={styles.input}
+                  onChangeText={textChange}
+                  value={newGoal}
+                  placeholder="Add a Task"
+              />
+              <Button style={styles.button} title="Submit" onPress={addGoal}/>
+          </View>
+      </>
 
 
-    <View style={styles.container}>
-      <Text style={styles.title}>App.js</Text>
-      <FlatList
-          data={sampleGoals}
-          renderItem={renderItem}
-          keyExtractor={(item) => item}
-      />
-      <TextInput
-          style={styles.input}
-          onChangeText={onChangeText}
-          value={text}
-          placeholder="add a task"
-      />
-      <Button
-          style={styles.button}
-          title="Submit"
-      />
 
-      <StatusBar style="auto" />
-
-    </View>
   );
 }
 
